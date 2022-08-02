1922a874d201e14ab23d1b8e12090eac36e76eb3
Alex Dong <team.magiccart@gmail.com>
2022-08-02 15:22:59 +0700

    Create remove_dot_slash.sh

---
+++ find/remove_dot_slash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# @Author: nguyen
+# @Date:   2022-07-07 14:54:44
+# @Last Modified by:   nguyen
+# @Last Modified time: 2022-07-07 16:48:23
+
+# List file or directy level 1 also remove "./" in path file
+
+echo 'Result Find:'
+
+# solution 1:
+find . -type f -not -path '*/.*' | sed 's|^./||'
+
+# solution 2:
+find . -type f -not -path '*/.*' | echo `sed 's|^./||'`
+
+# solution 3:
+find ~+ -type f -not -path '*/.*'
+
+# solution 4:
+find "$(cd ..; pwd)" -type f -not -path '*/.*'
