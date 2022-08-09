90eb79f74f8afe4702145c3464bbbc609fa922ce
Alex Dong <team.magiccart@gmail.com>
2022-08-09 18:49:11 +0700

    Update print-bash-arguments.sh

--- print-bash-arguments.sh
+++ print-bash-arguments.sh
@@ -7,6 +7,13 @@
 # https://stackoverflow.com/questions/43171648/sed-gives-sed-cant-read-no-such-file-or-directory
 # For support on both OSX and Linux, I use a simple if check to see if the bash script is running on OSX or Linux, and adjust the command's -i argument based on that.
 
+
+for i in "$@"; do
+    echo "$i"
+done
+
+# or 
+
 for (( i=$#;i>0;i-- ));do
         echo "${!i}"
 done
