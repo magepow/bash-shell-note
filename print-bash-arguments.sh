fa4c28241d80eb649595bc4c07e558b04cd701b1
Alex Dong <team.magiccart@gmail.com>
2022-08-09 18:46:16 +0700

    Create print-bash-arguments.sh

---
+++ print-bash-arguments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# @Author: nguyen
+# @Date:   2022-07-07 14:54:44
+# @Last Modified by:   Alex Dong
+# @Last Modified time: 2022-08-09 18:44:50
+
+# https://stackoverflow.com/questions/43171648/sed-gives-sed-cant-read-no-such-file-or-directory
+# For support on both OSX and Linux, I use a simple if check to see if the bash script is running on OSX or Linux, and adjust the command's -i argument based on that.
+
+for (( i=$#;i>0;i-- ));do
+        echo "${!i}"
+done
