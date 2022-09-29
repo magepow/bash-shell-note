e32b59855cb3fc37e7c9831cb85d57731c512d7f
Alex Dong <team.magiccart@gmail.com>
2022-09-29 12:04:36 +0700

    Remove dot in result

--- snippets/listDir.sh
+++ snippets/listDir.sh
@@ -4,34 +4,36 @@
 # @Last Modified by:   nguyen
 # @Last Modified time: 2022-07-07 16:48:23
 
+# not use for in with result command find because can error in cash name container space
+
 function ListDir()
 {
     echo 'Result find non recursively:'
-    files=`find . -maxdepth 1 -type d -not -path '*/.*' | sed 's|^./||'`
+    files=`find . -maxdepth 1 -type d ! -name . | sed 's|^./||'`
     echo $files
 }
 
 function ListDirHidden()
 {
     echo 'Result find Hidden directory:'
-    files=`find . -type d -name ".*" `
+    files=`find . -type d -name ".*" ! -name . `
     echo $files
 }
 
 function ListDirRecursive()
 {
     echo 'Result find recursively:'
-    files=`find . -type d -not -path '*/.*' | sed 's|^./||'`
+    files=`find . -type d ! -name . | sed 's|^./||'`
     echo $files
 }
 
 function ListDirRecursiveSameName()
 {
-    echo 'Result find recursively:'
-    files=`find . -type d -not -path '*/.*' | sed 's|^./||'`
+    echo 'Result find Same Name recursively:'
+    files=`find . -type d -name ".git" ! -name . | sed 's|^./||'`
     echo $files
 }
-find . -type d -name ".git"
+
 
 ListDir
 ListDirHidden
