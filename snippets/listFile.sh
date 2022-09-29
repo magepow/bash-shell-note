3089111dbb5afcdd4998789f36f9dee66cfdde4d
Alex Dong <team.magiccart@gmail.com>
2022-09-29 11:52:24 +0700

    Update listFile.sh

--- snippets/listFile.sh
+++ snippets/listFile.sh
@@ -4,6 +4,8 @@
 # @Last Modified by:   nguyen
 # @Last Modified time: 2022-07-07 16:48:23
 
+# not use for in with result command find because can error in cash name container space
+
 function ListFile()
 {
     echo 'Result find non recursively:'
@@ -15,6 +17,13 @@ function ListFileHidden()
 {
     echo 'Result find Hidden Files:'
     files=`find . -type f -name ".*" `
+    echo $files
+}
+
+function ListFileRecursive()
+{
+    echo 'Result find recursively:'
+    files=`find . -type f -not -path '*/.*' | sed 's|^./||'`
     echo $files
 }
 
