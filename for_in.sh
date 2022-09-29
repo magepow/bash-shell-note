f8dce34d604aee53d5eba3214cd0bacab1fa643d
Alex Dong <team.magiccart@gmail.com>
2022-09-29 13:38:33 +0700

    Update for_in.sh

--- for_in.sh
+++ for_in.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# while do
+# https://unix.stackexchange.com/questions/171677/cd-into-all-directories-execute-command-on-files-in-that-directory-and-return
 for d in ./*/ ; 
     do (
         cd "$d" && pwd
