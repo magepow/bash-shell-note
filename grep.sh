8cbdbc6ffe3d84242ed71e2e98d2c6c59919e0b3
Alex Dong <team.magiccart@gmail.com>
2023-05-12 11:44:25 +0700

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Find string in file and extension file
+# https://stackoverflow.com/questions/16956810/how-can-i-find-all-files-containing-specific-text-string-on-linux
 
 # Find all file exist string
 grep -rnw './' -e 'string_need_find'
 
 # Find only file .php  exist 'string_need_find'
 grep -rnw './' --include \*.php -e 'string_need_find'
+
+
+grep --include=\*.{php,html} -rnw '/path/to/somewhere/' -e "string_need_find"
+
+grep --exclude=\*.o -rnw '/path/to/somewhere/' -e "pattern"
