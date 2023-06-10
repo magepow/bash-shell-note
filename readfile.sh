56bd36c9335b7daee1c0def1f6793d56cd3b9336
Alex Dong <team.magiccart@gmail.com>
2023-06-10 11:20:19 +0700

    Update readfile.sh

--- readfile.sh
+++ readfile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 input=".env"
 while IFS= read -r line
 do
-  echo "$line"
+    echo "$line"
 done < "$input"
 
 
@@ -21,3 +22,18 @@ while IFS= read -r line
 do
     echo "$line"
 done < <(mysql -uroot -pXXXXXX -e "SHOW DATABASES LIKE 'wp2cart%'")
+
+
+listDatabase=""
+while IFS= read -r;
+do
+    ((i++)) && listDatabase="${listDatabase} $REPLY";
+done < <(mysql -uroot -pXXXXXX -e "SHOW DATABASES LIKE 'wp2cart%'")
+
+IFS=' ' #setting space as delimiter
+read -ra ADDR <<<"$listDatabase" #reading str as an array as tokens separated by IFS
+
+for i in "${ADDR[@]}"; #accessing each element of array
+do
+    echo "$i"
+done
